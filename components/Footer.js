0136eafbb000df2bfa908e5116ce552e49f1ffdb
Data Ng <data@connect.hku.hk>
2022-01-01 18:39:40 +0800

    sorted the json from API in terms of the metadata.name

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer(prop){
+    return(
+        <div>
+            <h2>Footer {prop.contract_address}</h2>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -22,44 +22,12 @@ import { Heading, Link, Flex, Box, Button, Divider } from "@chakra-ui/react";
 
 import { Image } from "@chakra-ui/react";
 
-import { SimpleGrid } from '@chakra-ui/react'
+import { SimpleGrid } from '@chakra-ui/react';
+
+import Data from "./api/Data";
 
 export default function Home() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [photos, setPhotos] = useState([]); 
-
-  const contract_address = "0xCfa71823FEc407Ccb2EA64a8ff265B41fC2f7707";
   
-  const opensea_link = "https://opensea.io/assets/matic/"+contract_address+"/";
-
-
-  useEffect(() => {
-    // fetch("https://api.nftport.xyz/v0/nfts/0xD4b3f1c0C67a493477F1eBc5d04c12eB4de168FE?chain=polygon&include=metadata", {
-      fetch("https://api.nftport.xyz/v0/nfts/"+ contract_address +"?chain=polygon&include=metadata", {
-          "method": "GET",
-          "headers": {
-            "Content-Type": "application/json",
-            "Authorization": "ff7dd013-272d-4e38-8b27-b1ad8ad4ea47"
-          }
-          })
-      .then(res => res.json())
-      .then(
-        (data) => {
-          setIsLoaded(true);
-          setPhotos(data.nfts);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  }, [])
-if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
     return (
 
     <Box>
@@ -81,38 +49,14 @@ if (error) {
         <Heading as="h2" size="lg" marginY="1rem">
           from contract
         </Heading>
-        <Button
-          variantColor="blue"
-          margin="3rem"
-          onClick={() => Router.push(`/user/${getRandomInt(40)}`)}
-        >
-          Check out a random user
-        </Button>
-        </Flex>
 
-        <Box>
-        <SimpleGrid minChildWidth='250px' spacing='10px'>
-        {photos.map((item, index) => (
-              <div key={index}>
-                <a href={opensea_link+item.token_id} target="_blank" rel="noreferrer">
-                  <div>
-                    <Image src={item.cached_file_url} 
-                            width='300px' 
-                            height='150px' 
-                            alt={item.metadata.description}
-                            quality={2}
-                            />
-                            
-                  </div>
-                  <div>
-                    {item.metadata.name}
-                  </div>
-                </a>
-              </div>
-            ))}
-        </SimpleGrid>      
-      </Box>
+        </Flex>
+        <Box m={[2, 55]} alignItems="center">
+          <Data contract_address="0xCfa71823FEc407Ccb2EA64a8ff265B41fC2f7707"></Data>
+        </Box>
+
+        
     </Box>
     );
   }
-}
+

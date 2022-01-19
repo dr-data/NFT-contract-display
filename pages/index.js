9b87068894f41dc3d611d2e04bf29b0b7badd9b6
Data Ng <data@connect.hku.hk>
2022-01-19 15:22:44 +0800

    Adding iframe of drop

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,7 @@ export default function home() {
         <SimpleGrid columns={2} spacing={10}>
         <Box m={[2, 55]} alignItems="center">
           <Iframe
-            src="https://cloudflare-ipfs.com/ipfs/bafybeib6q6q2a4ibiwfq657suynze7r4a3kyh6cuhvkga2sc4gjez2rz2y?contract=0x5B1c5EfcC3ce4BD862cb5c892712be146857358F&chainId=137&tokenId=0&relayUrl=https://api.defender.openzeppelin.com/autotasks/8c35b8bd-7865-4404-b9e2-26001e54346a/runs/webhook/771a7ae1-ce84-4975-b343-47f2de566a57/9vDZeN6r1RbkoTqDekTmYp"
+            src="https://cloudflare-ipfs.com/ipfs/bafybeib6q6q2a4ibiwfq657suynze7r4a3kyh6cuhvkga2sc4gjez2rz2y?contract=0x5B1c5EfcC3ce4BD862cb5c892712be146857358F&chainId=137&tokenId=0"
             width="100%"
             height="422px"
             frameBorder="0"
@@ -57,7 +57,7 @@ export default function home() {
         </Box>
         <Box m={[2, 55]} alignItems="center">
         <Iframe
-            src="https://cloudflare-ipfs.com/ipfs/bafybeib6q6q2a4ibiwfq657suynze7r4a3kyh6cuhvkga2sc4gjez2rz2y?contract=0x5B1c5EfcC3ce4BD862cb5c892712be146857358F&chainId=137&tokenId=1&relayUrl=https://api.defender.openzeppelin.com/autotasks/8c35b8bd-7865-4404-b9e2-26001e54346a/runs/webhook/771a7ae1-ce84-4975-b343-47f2de566a57/9vDZeN6r1RbkoTqDekTmYp"
+            src="https://cloudflare-ipfs.com/ipfs/bafybeib6q6q2a4ibiwfq657suynze7r4a3kyh6cuhvkga2sc4gjez2rz2y?contract=0x5B1c5EfcC3ce4BD862cb5c892712be146857358F&chainId=137&tokenId=1"
             width="100%"
             height="422px"
             frameBorder="0"

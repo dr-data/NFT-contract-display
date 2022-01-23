0e37c31d19f649088c9378b9ffa391a67065f59b
Data Ng <data@connect.hku.hk>
2022-01-23 19:56:51 +0800

    hello

--- pages/index.js
+++ pages/index.js
@@ -77,7 +77,7 @@ export default function home() {
           <Faq data={data}
               styles={{
                 titleTextColor: "green",
-                rowTitleColor: "mediumseagreen"
+                rowTitleColor: "mediumseagreen",
                 rowContentColor: "grey"
 
             }} 

a555ccdbde9abc8310a1f4a086b5e87a0afe3fb4
Data NG <dr-data@users.noreply.github.com>
2022-01-24 15:08:22 +0800

    code sandbox

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,16 @@
-
 // need to add the UI
 // https://chakra-ui.com/guides/getting-started/nextjs-guide
 //
 // Sorting the json
 // According to the filename
 // https://stackoverflow.com/questions/47998188/how-to-sort-an-object-alphabetically-within-an-array-in-react-js/47998260
-// 
+//
 // Adding two api
 // 1. fetch the metadata of the NFTs
 // 2. fetch the mint to address
 // https://egghead.io/lessons/react-fetch-data-from-an-api-on-the-server-side-with-getserversideprops-in-next-js
 //
 // putting the API KEY to the env
-
 
 //https://raptis.wtf/blog/create-a-navbar-with-chakra-ui-react/
 
@@ -21,20 +19,20 @@
 //https://www.bunnywarriors.io/home
 //https://novalaunch.art/#about
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import NextLink from "next/link";
 import Router from "next/router";
 import { Heading, Link, Flex, Box, Button, Divider } from "@chakra-ui/react";
 
 import { Image } from "@chakra-ui/react";
 
-import { SimpleGrid } from '@chakra-ui/react';
+import { SimpleGrid } from "@chakra-ui/react";
 
-import { Grid, GridItem } from '@chakra-ui/react'
+import { Grid, GridItem } from "@chakra-ui/react";
 
-import Iframe from 'react-iframe'
+import Iframe from "react-iframe";
 
-import Faq from 'react-faq-component';
+import Faq from "react-faq-component";
 
 import Nav from "../components/Nav";
 
@@ -53,41 +51,40 @@ const data = {
     },
     {
       title: "Nunc maximus, magna at ultricies elementum",
-      content: "Nunc maximus, magna at ultricies elementum, risus turpis vulputate quam."
+      content:
+        "Nunc maximus, magna at ultricies elementum, risus turpis vulputate quam."
     },
     {
       title: "Curabitur laoreet, mauris vel blandit fringilla",
-      content: "Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc"
+      content:
+        "Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc"
     },
     {
       title: "What is the package version",
       content: "v1.0.5"
-    }]
-}
-
+    }
+  ]
+};
 
 export default function home() {
-    return (
+  return (
+    <Box bg="#FFFBF6">
+      <HeadingSection />
+      <div>
+        <Faq
+          data={data}
+          styles={{
+            titleTextColor: "green",
+            rowTitleColor: "mediumseagreen",
+            rowContentColor: "grey"
+          }}
+          config={{
+            arrowIcon: "V"
+          }}
+        />
+      </div>
 
-
-
-    <Box bg='#FFFBF6'>
-        <HeadingSection />
-        <div>
-          <Faq data={data}
-              styles={{
-                titleTextColor: "green",
-                rowTitleColor: "mediumseagreen",
-                rowContentColor: "grey"
-
-            }} 
-            config={{
-                arrowIcon: "V",
-            }}
-          />
-        </div>
-
-        {/* <SimpleGrid minChildWidth='120px' columns={2} spacing={10}>
+      {/* <SimpleGrid minChildWidth='120px' columns={2} spacing={10}>
         <Box m={[2, 55]} alignItems="center">
         <Iframe
             src="https://cloudflare-ipfs.com/ipfs/bafybeib6q6q2a4ibiwfq657suynze7r4a3kyh6cuhvkga2sc4gjez2rz2y?contract=0x5B1c5EfcC3ce4BD862cb5c892712be146857358F&chainId=137&tokenId=0"
@@ -107,12 +104,7 @@ export default function home() {
         </Box>
         </SimpleGrid> */}
 
-
-       
-
-
-        <Footer />
+      <Footer />
     </Box>
-    );
-  }
-
+  );
+}
